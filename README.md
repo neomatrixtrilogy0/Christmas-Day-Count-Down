6c410f4151651a350fceaeb9b7f55ef8a21c9337
neo <neo@neo>
2021-03-23 22:54:37 -0400

    Added a readme file and finished with this project after finalizing the js portion of it.

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is about how many days hours minutes and seconds are left until Christmas day.
--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const newYears = '25 Dec 2021';
 
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
 
 function countDown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
-    const seconds = (newYearsDate - currentDate) / 1000;
-    const days = Math.floor(seconds / 3600 / 24);
-    const hours = days * 24
+    const Totalseconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(Totalseconds / 3600 / 24);
+    const hours = Math.floor(Totalseconds / 3600) % 24;
+    const minutes = Math.floor(Totalseconds / 60) % 60;
+    const seconds = Math.floor(Totalseconds) % 60;
+   
 
-    console.log(hours)
+    daysElement.innerHTML = days;
+    hoursElement.innerText = timeFormat(hours);
+    minutesElement.innerHTML = timeFormat(minutes);
+    secondsElement.innerHTML = timeFormat(seconds);
     
 }
-
+function timeFormat(time){
+    return time < 10 ?(`0${time}`) : time;
+}
 countDown()
 
-// setInterval(countDown, 1000)
+setInterval(countDown, 1000)
